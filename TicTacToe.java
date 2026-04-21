60245a9a3f03244e9b7b2a422c97243a5576c0c6
Kata Sai Kranthu Reddy <kskreddy2k7@gmail.com>
2026-04-21 09:42:56 +0530

    Implement move validation in TicTacToe
    Added a method to validate moves in the TicTacToe game.

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,1 +1,37 @@
 
+public class TicTacToe {
+
+    static char[][] board = {
+        {'-', '-', '-'},
+        {'-', '-', '-'},
+        {'-', '-', '-'}
+    };
+
+    // UC5: Validate move
+    public static boolean isValidMove(int row, int col) {
+
+        // Check bounds
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+
+        // Check if cell is empty
+        if (board[row][col] != '-') {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int row = 1;
+        int col = 1;
+
+        if (isValidMove(row, col)) {
+            System.out.println("Valid move");
+        } else {
+            System.out.println("Invalid move");
+        }
+    }
+}
