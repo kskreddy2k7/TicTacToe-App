6dd977b320a8f8c312cf32487296d59b75cf7ede
Kata Sai Kranthu Reddy <kskreddy2k7@gmail.com>
2026-04-28 17:53:23 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class TicTacToe {
 
     static char[][] board = {
@@ -6,31 +8,43 @@ public class TicTacToe {
         {'-', '-', '-'}
     };
 
-    // UC5: Validate move
-    public static boolean isValidMove(int row, int col) {
-
-        // Check bounds
-        if (row < 0 || row > 2 || col < 0 || col > 2) {
-            return false;
-        }
-
-        // Check if cell is empty
-        if (board[row][col] != '-') {
-            return false;
-        }
-
-        return true;
-    }
+    static char computerSymbol = 'O';
 
     public static void main(String[] args) {
+        computerMove();
+        printBoard();
+    }
 
-        int row = 1;
-        int col = 1;
+    // UC7: Computer makes random valid move
+    static void computerMove() {
 
-        if (isValidMove(row, col)) {
-            System.out.println("Valid move");
-        } else {
-            System.out.println("Invalid move");
+        Random rand = new Random();
+        int row, col;
+
+        while (true) {
+
+            int slot = rand.nextInt(9) + 1; // generate 1–9
+
+            // Convert slot → row & column
+            row = (slot - 1) / 3;
+            col = (slot - 1) % 3;
+
+            // Check if cell is empty
+            if (board[row][col] == '-') {
+                board[row][col] = computerSymbol;
+                System.out.println("Computer chose slot: " + slot);
+                break;
+            }
+        }
+    }
+
+    // Display board
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 }
