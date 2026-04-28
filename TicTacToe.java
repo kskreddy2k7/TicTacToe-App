d7f3af0d1d4a0c0ba8d4c3ac1202c272ff1435a0
Kata Sai Kranthu Reddy <kskreddy2k7@gmail.com>
2026-04-28 17:57:39 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -6,31 +6,99 @@ public class TicTacToe {
         {'-', '-', '-'}
     };
 
-    // UC5: Validate move
-    public static boolean isValidMove(int row, int col) {
-
-        // Check bounds
-        if (row < 0 || row > 2 || col < 0 || col > 2) {
-            return false;
-        }
-
-        // Check if cell is empty
-        if (board[row][col] != '-') {
-            return false;
-        }
-
-        return true;
-    }
+    static boolean isHumanTurn = true;
+    static boolean gameOver = false;
 
     public static void main(String[] args) {
 
-        int row = 1;
-        int col = 1;
+        while (!gameOver) {
 
-        if (isValidMove(row, col)) {
-            System.out.println("Valid move");
-        } else {
-            System.out.println("Invalid move");
+            printBoard();
+
+            if (isHumanTurn) {
+                System.out.println("Human Turn (X)");
+                // demo move (you can replace with input later)
+                placeMove(0, 0, 'X');
+            } else {
+                System.out.println("Computer Turn (O)");
+                computerMove();
+            }
+
+            // check game state
+            if (checkWin('X') || checkWin('O')) {
+                printBoard();
+                System.out.println("Game Over - We have a winner!");
+                gameOver = true;
+            } else if (isBoardFull()) {
+                printBoard();
+                System.out.println("Game Over - It's a draw!");
+                gameOver = true;
+            }
+
+            // switch turn
+            isHumanTurn = !isHumanTurn;
         }
+    }
+
+    // UC6
+    static void placeMove(int row, int col, char symbol) {
+        if (board[row][col] == '-') {
+            board[row][col] = symbol;
+        }
+    }
+
+    // UC7
+    static void computerMove() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    board[i][j] = 'O';
+                    return;
+                }
+            }
+        }
+    }
+
+    // check win
+    static boolean checkWin(char symbol) {
+
+        // rows & columns
+        for (int i = 0; i < 3; i++) {
+            if ((board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) ||
+                (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol)) {
+                return true;
+            }
+        }
+
+        // diagonals
+        if ((board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) ||
+            (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // check draw
+    static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // display
+    static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
